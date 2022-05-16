3eabecb8170724c6cfbf5dc5207e8de49c55600a
toddbrentlinger <toddbrentlinger@gmail.com>
2022-05-15 17:46:03 -0700

    first commit

---
+++ initial-page-load.bundle.js
@@ -0,0 +1,30 @@
+"use strict";
+(self["webpackChunkodin_project_restaurant_page"] = self["webpackChunkodin_project_restaurant_page"] || []).push([["initial-page-load"],{
+
+/***/ "./src/initial-page-load.js":
+/*!**********************************!*\
+  !*** ./src/initial-page-load.js ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ initialPageLoad)
+/* harmony export */ });
+function initialPageLoad() {
+    const element = document.createElement('div');
+
+    element.innerHTML = 'Initial Page Load';
+
+    return element;
+}
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/initial-page-load.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5pdGlhbC1wYWdlLWxvYWQuYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7QUFBZTtBQUNmO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL29kaW4tcHJvamVjdC1yZXN0YXVyYW50LXBhZ2UvLi9zcmMvaW5pdGlhbC1wYWdlLWxvYWQuanMiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gaW5pdGlhbFBhZ2VMb2FkKCkge1xyXG4gICAgY29uc3QgZWxlbWVudCA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoJ2RpdicpO1xyXG5cclxuICAgIGVsZW1lbnQuaW5uZXJIVE1MID0gJ0luaXRpYWwgUGFnZSBMb2FkJztcclxuXHJcbiAgICByZXR1cm4gZWxlbWVudDtcclxufSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
