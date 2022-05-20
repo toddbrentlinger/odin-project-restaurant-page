710767c0c7d6252810e72938aaecb2277def5efe
toddbrentlinger <toddbrentlinger@gmail.com>
2022-05-19 19:22:55 -0700

    Finish animation for logo on home page

--- src/animated-logo.js
+++ src/animated-logo.js
@@ -2,6 +2,8 @@ import logoTextSign from './img/logo-text-sign.png';
 import logoRooster from './img/logo-rooster.png';
 import logoTrees from './img/logo-trees.png';
 import logoImageSign from './img/logo-image-sign.png';
+import { createElement } from './utilities.js';
+import './animated-logo.scss';
 
 export default function AnimatedLogoComponent() {
     const _createImgElement = function(src, alt = '', className = '') {
@@ -13,12 +15,14 @@ export default function AnimatedLogoComponent() {
     }
 
     const render = function() {
-        const animatedLogoElement = document.createElement('div');
-        animatedLogoElement.id = 'animated-logo';
+        const animatedLogoElement = createElement('div', {id: 'animated-logo'});
 
-        animatedLogoElement.appendChild(_createImgElement(logoImageSign, '', 'logo-image-sign'));
-        animatedLogoElement.appendChild(_createImgElement(logoTrees, '', 'logo-trees'));
-        animatedLogoElement.appendChild(_createImgElement(logoRooster, '', 'logo-rooster'));
+        animatedLogoElement.appendChild(createElement('div', {'class': 'animated-logo-back'}, 
+            _createImgElement(logoImageSign, '', 'logo-image-sign'),
+            _createImgElement(logoTrees, '', 'logo-trees'),
+            _createImgElement(logoRooster, '', 'logo-rooster')
+        ));
+
         animatedLogoElement.appendChild(_createImgElement(logoTextSign, '', 'logo-text-sign'));
 
         return animatedLogoElement;
